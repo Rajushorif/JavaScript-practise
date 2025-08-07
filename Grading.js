54d4e2958a4d903764bcfb424fafe26a67901724
RajuShorif <rajushorif48@gmail.com>
2025-08-07 09:56:59 +0600

    Grading method

---
+++ Grading.js
@@ -0,0 +1,32 @@
+// Grading System Example
+// let score = 80;
+
+// if (score >= 90) {
+//   console.log("A+");
+// } else if (score >= 80) {
+//   console.log("A");
+// } else if (score >= 70) {
+//   console.log("B");
+// } else if (score >= 60) {
+//   console.log("C");
+// } else if (score >= 50) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// Ternary Operator Example
+let score = 85;
+let grade =
+  score >= 90
+    ? "A+"
+    : score >= 80
+    ? "A"
+    : score >= 70
+    ? "B"
+    : score >= 60
+    ? "c"
+    : score >= 50
+    ? "D"
+    : "F";
+console.log(grade);
